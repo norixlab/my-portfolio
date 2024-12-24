44818bf71e037fb0da8fe19be6c9402a39dd7c72
Norixlab <s.taydakov@gmail.com>
2024-12-24 23:51:43 +0400

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -53,6 +53,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|yandex_14718eb633b29229.html).*)",
   ],
 };
